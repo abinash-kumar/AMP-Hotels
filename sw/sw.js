c6c3407a92f522d11cd1eca7818187aeb616a2e2
abinash-kumar <abinashlv@gmail.com>
2017-06-13 18:24:26 +0530

    update

--- sw/sw.js
+++ sw/sw.js
@@ -1,7 +1,7 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw/sw.js', { scope: 'sw/sw.js' }).then(function(reg) {
+  navigator.serviceWorker.register('./sw/sw.js', { scope: './sw/sw.js' }).then(function(reg) {
 
     if(reg.installing) {
       console.log('Service worker installing');
